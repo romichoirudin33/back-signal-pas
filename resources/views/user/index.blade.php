1e8c1d7b00dc731026ddbfab09740150c5907ff2
romichoirudin33 <romichoirudin33@gmail.com>
2020-09-09 16:55:30 +0800

    fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -27,22 +27,30 @@
                                     <button class="btn btn-outline-info btn-sm dropdown-toggle"
                                             type="button" id="dropdownMenuButton"
                                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear-fill"
+                                             fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                  d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
                                         </svg>
                                         Pengaturan
                                     </button>
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                                         <h6 class="dropdown-header">Filter</h6>
                                         <a class="dropdown-item" href="{{ route('user.index') }}">Semua User</a>
-                                        <a class="dropdown-item" href="{{ route('user.index', ['jenis'=> 'admin']) }}">User Admin</a>
-                                        <a class="dropdown-item" href="{{ route('user.index', ['jenis'=> 'sipir']) }}">User Sipir</a>
+                                        <a class="dropdown-item" href="{{ route('user.index', ['jenis'=> 'admin']) }}">User
+                                            Admin</a>
+                                        <a class="dropdown-item" href="{{ route('user.index', ['jenis'=> 'sipir']) }}">User
+                                            Sipir</a>
                                         <div class="dropdown-divider"></div>
                                         <h6 class="dropdown-header">Export</h6>
-                                        <a class="dropdown-item" target="_blank" href="{{ route('user.index', ['export' => 'xls','jenis' => request('jenis')]) }}">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                                                <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                        <a class="dropdown-item" target="_blank"
+                                           href="{{ route('user.index', ['export' => 'xls','jenis' => request('jenis')]) }}">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download"
+                                                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                      d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                                <path fill-rule="evenodd"
+                                                      d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
                                             </svg>
                                             Excel
                                         </a>
@@ -121,15 +129,17 @@
                                                 @endif
                                             </td>
                                             <td class="text-center">
-                                                {{ $i->warden['score'] }}
-                                                @if($i->warden['score'] != "")
-                                                    <br>
-                                                    @if($i->warden['score'] < 45 )
-                                                        <span class="badge badge-danger">Rendah</span>
-                                                    @elseif($i->warden['score'] <= 135 )
-                                                        <span class="badge badge-warning">Sedang</span>
-                                                    @else
-                                                        <span class="badge badge-success">Tinggi</span>
+                                                @if(!$i->is_admin)
+                                                    {{ $i->warden['score'] }}
+                                                    @if($i->warden['score'] != "")
+                                                        <br>
+                                                        @if($i->warden['score'] < 45 )
+                                                            <span class="badge badge-danger">Rendah</span>
+                                                        @elseif($i->warden['score'] <= 135 )
+                                                            <span class="badge badge-warning">Sedang</span>
+                                                        @else
+                                                            <span class="badge badge-success">Tinggi</span>
+                                                        @endif
                                                     @endif
                                                 @endif
                                             </td>
