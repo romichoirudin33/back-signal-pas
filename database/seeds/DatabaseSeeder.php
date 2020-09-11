4c0153295d3c87daa10e05fbd20ce8b22119920a
romichoirudin33 <romichoirudin33@gmail.com>
2020-09-12 00:15:03 +0800

    fix new

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserAdminSeeder::class);
+
 //    	$this->call(UserSeeder::class);
 //    	$this->call(PostsSeeder::class);
     }
